9c962113c9e9ca94b4212f4a320295fa6ed51440
Jones Mbindyo <jonesmbindyo@gmail.com>
2021-09-28 14:18:48 +0300

    main activity

--- app/src/main/java/com/extrainchltd/posprint/MainActivity.java
+++ app/src/main/java/com/extrainchltd/posprint/MainActivity.java
@@ -1,14 +1,56 @@
 package com.extrainchltd.posprint;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.extrainchltd.posprint.databinding.ActivityMainBinding;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnPrint.setOnClickListener(v -> printIt(binding.editTextTextPersonName.getText().toString().trim()));
+    }
+
+    private void printIt(String thisData) {
+        BluetoothSocket socket = null;
+        byte [] data = thisData.getBytes();
+
+        //Get BluetoothAdapter
+        BluetoothAdapter btAdapter = BluetoothUtil.getBTAdapter();
+        if(btAdapter == null) {
+            Toast.makeText(getBaseContext(), "Open Bluetooth", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Get sunmi InnerPrinter BluetoothDevice
+        BluetoothDevice device = BluetoothUtil.getDevice(btAdapter);
+        if(device == null) {
+            Toast.makeText(getBaseContext(), "Make Sure Bluetooth have InterPrinter", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            socket = BluetoothUtil.getSocket(device);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BluetoothUtil.sendData(data, socket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
